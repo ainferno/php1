325d2f67a81efc976a4780a537454c7d58ecbdc7
Ainferno <vasily.nichiporenko@ya.ru>
2016-07-19 08:07:00 +0300

    Creation of first lesson1

---
+++ lesson1/homework1.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>Ы</title>
+</head>
+<body>
+
+<?php
+
+echo ($a = 2);
+echo '<br>';
+$x = ($y = 12) - 8; 
+echo $x;
+echo '<br>';
+var_dump(1 == 1.0);
+echo '<br>';
+var_dump(1 === 1.0); 
+echo '<br>';
+var_dump('02' == 2);
+echo '<br>';
+var_dump('02' === 2);
+echo '<br>';
+var_dump('02' == '2');
+
+?>
+
+<body>
+</html>
